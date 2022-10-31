466d6fd7d9a8b7c40d8144856d39e40f0b4bf5cd
lovelin0523 <18356201582@163.com>
2022-10-31 14:55:52 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
 const path = require('path')
 
 let params = {
-	entry: './src/index.js', //入口文件
-	output: { //输出
-		path: path.resolve(__dirname, 'dist'),
-		filename: "rem-fit.js",
-		library: 'RemFit',
-		libraryTarget: "umd"
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: "babel-loader"
-		}]
-	}
+    //入口文件
+    entry: './src/index.js',
+    //输出
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'rem-fit.js',
+        library: 'RemFit',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
 }
 
-if (process.env.NODE_ENV == "development") {
-	params.devtool = 'eval-source-map' //调试可看到源码
+if (process.env.NODE_ENV == 'development') {
+    params.devtool = 'eval-source-map' //调试可看到源码
 }
 
 module.exports = params
